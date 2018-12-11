fd0aa9a6e73bc8213ad8239df817fc89b0b4a6b5
Ana-Maria Melinte <34279579+AnMa12@users.noreply.github.com>
2018-12-11 22:25:01 +0200

    course 6

---
+++ myFirstMVC/myFirstMVC/myFirstMVC/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace myFirstMVC.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public string Index(string name, int? id)
+        {
+            string response = "Hello" + name + "!";
+            if(id != null)
+            {
+                response += "; ID = " + id.ToString();
+            }
+
+            return response;            
+            //return View();
+        }
+    }
+}
